44486b94bd74bf2b585003bb42945c163f7c1156
gusttavoo123 <gustavo.agalvao@hotmail.com>
2023-05-04 16:50:32 -0300

    teste de branchs

--- test.js
+++ test.js
@@ -1,3 +1,14 @@
 let value = 1;
 let value2 = 2;
-aaaaa
+
+let value3= 1;
+let value4 = 2;
+
+let value5 = 1;
+let value6 = 2;
+
+let value7 = 1;
+let value8 = 2;
+
+let value9 = 1;
+let value10 = 2;
